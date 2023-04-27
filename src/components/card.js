4fe1eb5783d0ae09f92bbe1bc0060248da2340d6
Osipov Alexander <saxmail0722@gmail.com>
2023-04-28 00:36:29 +0500

    add webpack

--- src/components/card.js
+++ src/components/card.js
@@ -1,34 +1,68 @@
-import {addCard} from './utils.js'
+// import {addCard} from './utils.js'
 
-const initialCards = [
-{
-  name: 'Алтай',
-  link: './images/altai-full.jpg'
-},
-{
-  name: 'Калмыкия',
-  link: './images/kal-mykia.jpg'
-},
-{
-  name: 'Камчатка',
-  link: './images/cum-chatka.jpg'
-},
-{
-  name: 'Карелия',
-  link: './images/kareliya.jpg'
-},
-{
-  name: 'Байкал',
-  link: './images/buy-kal.jpg'
-},
-{
-  name: 'Карачаево-Черкессия',
-  link: './images/cherkes.jpg'
-},
-];
+// const altay = new URL('./images/altai-full.jpg', import.meta.url);
+// const kalmykia = new URL('./images/kal-mykia.jpg', import.meta.url);
+// const cumchatka = new URL('./images/cum-chatka.jpg', import.meta.url);
+// const karelia = new URL('./images/kareliya.jpg', import.meta.url);
+// const buykal = new URL('./images/buy-kal.jpg', import.meta.url);
+// const cherkash = new URL('./images/cherkes.jpg', import.meta.url);
+
+// const initialCards = [
+// {
+//   name: 'Алтай',
+//   link: altay
+// },
+// {
+//   name: 'Калмыкия',
+//   link: kalmykia
+// },
+// {
+//   name: 'Камчатка',
+//   link: cumchatka
+// },
+// {
+//   name: 'Карелия',
+//   link: karelia
+// },
+// {
+//   name: 'Байкал',
+//   link: buykal
+// },
+// {
+//   name: 'Карачаево-Черкессия',
+//   link: cherkash
+// },
+// ];
+
+// const initialCards = [
+//   {
+//     name: 'Алтай',
+//     link: './images/altai-full.jpg'
+//   },
+//   {
+//     name: 'Калмыкия',
+//     link: './images/kal-mykia.jpg'
+//   },
+//   {
+//     name: 'Камчатка',
+//     link: './images/cum-chatka.jpg'
+//   },
+//   {
+//     name: 'Карелия',
+//     link: './images/kareliya.jpg'
+//   },
+//   {
+//     name: 'Байкал',
+//     link: './images/buy-kal.jpg'
+//   },
+//   {
+//     name: 'Карачаево-Черкессия',
+//     link: './images/cherkes.jpg'
+//   },
+//   ];
 
 //создаем дефолтные карточки
 
-initialCards.forEach( ({name, link}) => {
-  addCard(name, link);
-});
+// initialCards.forEach( ({name, link}) => {
+//   addCard(name, link);
+// });
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import {addCardButton, cardPopup, cardTitleInput, cardLinkInput} from './components/modal.js'
 import {showInputError, hideInputError, checkInputValidity, hasInvalidInput, toggleButtonState, setEventListeners, enableValidation} from './components/validate.js'
 import {createCard, addCard, openPopup, closePopup} from './components/utils.js'
+import './pages/index.css'
 
-// добавляем закрытие попапов по escape и клику на оверлей
+// добавляем закрытие попапов по клику на оверлей
 
 (function closeOnOverlay () {
-  
-  // const popups = document.querySelectorAll('.popup')
-  // Array.from(popups).forEach(popupItem => {
-  //   addEventListener('keydown', evt => {
-  //     if (evt.key === 'Escape') { 
-  //       closePopup(popupItem)
-  //     }
-  //   })
-  // })
-  
-
-
     const overlays = document.querySelectorAll('.popup__overlay')
     Array.from(overlays).forEach(item => {
       item.addEventListener('click', () => closePopup(item.closest('.popup')))
@@ -40,7 +29,48 @@ cardPopup.addEventListener('submit', (evt) => {
   evt.target.reset();
 });
 
+// добавляем дефолтные карточки
+
+const altay = new URL('./images/altai-full.jpg', import.meta.url);
+const kalmykia = new URL('./images/kal-mykia.jpg', import.meta.url);
+const cumchatka = new URL('./images/cum-chatka.jpg', import.meta.url);
+const karelia = new URL('./images/kareliya.jpg', import.meta.url);
+const buykal = new URL('./images/buy-kal.jpg', import.meta.url);
+const cherkash = new URL('./images/cherkes.jpg', import.meta.url);
+
+const initialCards = [
+{
+  name: 'Алтай',
+  link: altay
+},
+{
+  name: 'Калмыкия',
+  link: kalmykia
+},
+{
+  name: 'Камчатка',
+  link: cumchatka
+},
+{
+  name: 'Карелия',
+  link: karelia
+},
+{
+  name: 'Байкал',
+  link: buykal
+},
+{
+  name: 'Карачаево-Черкессия',
+  link: cherkash
+},
+];
+
+initialCards.forEach( ({name, link}) => {
+  addCard(name, link);
+});
+
 // Включаем валидацию инпутов
 
 enableValidation();
+
 
